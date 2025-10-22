d2069d439ad2b65dc615ee9fdb76d68e98e8ed02
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-10-22 19:44:21 +0200

    create types for Level

--- app/api/docs/route.ts
+++ app/api/docs/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient, Level } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+import { Level } from "@/lib/types";
 
 const prisma = new PrismaClient();
 
